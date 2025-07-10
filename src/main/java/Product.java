68fd0861f058bef5937614702d260c13e520604c
Shahina <gozelllhesimovaaa@gmail.com>
2025-07-10 17:21:41 +0400

    product class

--- src/main/java/Product.java
+++ src/main/java/Product.java
@@ -1,2 +1,18 @@
 public class Product {
+
+    public String name;
+    public double price;
+    public String category;
+    public int stock;
+
+    public Product(String name, double price, String category, int stock) {
+        this.name = name;
+        this.price = price;
+        this.category = category;
+        this.stock = stock;
+    }
+
+    public String toString() {
+        return ("Product = name:%s\n price%d\n category%s\n stock%d\n");
+    }
 }
