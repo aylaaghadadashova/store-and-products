c05f7137bcf85b1f6f4afe1adc6152552c17e82d
Ayla <ayla.aghadadashova.std@bhos.edu.az>
2025-07-10 17:39:13 +0400

    first commit

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,7 +5,11 @@ public class Main {
     public static void main(String[] args) {
 
         Product product1 = new Product("apple",  0.50, "fruit", 12 );
-        System.out.println(product1.toString());
+        Product product2 = new Product("strawberry",  2.5, "fruit", 10 );
+        Store store = new Store("Rahat", "Baku", new Product[]{product1, product2});
+
+        store.applyDiscountToAll(10);
+        //System.out.println(product1.toString());
     }
 
 
--- src/main/java/Store.java
+++ src/main/java/Store.java
@@ -1,2 +1,36 @@
 public class Store {
+
+    //fields:
+    public String name;
+    public String address;
+    public Product[] products;
+
+    //constructors:
+    public Store(){};
+    public Store(String name, String address, Product[] products){
+        this.name = name;
+        this.address = address;
+        this.products =  products;
+    }
+
+    //methods:
+    public void printAllProducts(){
+        for(Product product : products){
+            System.out.println(product.toString());
+        }
+    }
+
+    public void printExpensiveProducts(double limit){
+        for(Product product : products){
+            if(product.price > limit) System.out.println(product.name);
+        }
+    }
+
+    public void applyDiscountToAll(double percent){
+        for(int i = 0; i<products.length; i++){
+            products[i].price *=(100-percent)/100;
+            System.out.println(products[i].price);
+        }
+    }
+
 }
